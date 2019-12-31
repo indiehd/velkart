454454e5ac5f626fb3751b212dd2f54d878e840d
Poppabear <servnx@gmail.com>
2019-12-31 10:33:38 -0500

    fix: Classname in attribute values migration table

--- database/migrations/2019_12_21_000011_create_attribute_values_table.php
+++ database/migrations/2019_12_21_000011_create_attribute_values_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAttributesTable extends Migration
+class CreateAttributeValuesTable extends Migration
 {
     /**
      * Run the migrations.
