961dfb17229cf5613929bae295e77d6f4ed62928
Poppabear <servnx@gmail.com>
2020-01-03 12:22:57 -0500

    chore: Remove unused method from UploadsFiles

--- src/Base/Traits/UploadsFiles.php
+++ src/Base/Traits/UploadsFiles.php
@@ -7,25 +7,6 @@ use Illuminate\Http\UploadedFile;
 trait UploadsFiles
 {
     /**
-     * Upload a single file in the server
-     *
-     * @param UploadedFile $file
-     * @param null $folder
-     * @param string $disk
-     * @param null $filename
-     * @return false|string
-     */
-    public function uploadOne(UploadedFile $file, $folder = null, $disk = 'public', $filename = null)
-    {
-        $name = !is_null($filename) ? $filename : str_random(25);
-        return $file->storeAs(
-            $folder,
-            $name . "." . $file->getClientOriginalExtension(),
-            $disk
-        );
-    }
-
-    /**
      * @param UploadedFile $file
      *
      * @param string $folder
