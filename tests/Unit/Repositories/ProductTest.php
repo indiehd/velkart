4de45a44b2f6b78060ba84650ce34825df8d5843
Ben Johnson <ben@indiehd.com>
2020-01-10 16:57:52 -0500

    tests: Improve veracity of test considerably

--- tests/Unit/Repositories/ProductTest.php
+++ tests/Unit/Repositories/ProductTest.php
@@ -2,16 +2,22 @@
 
 namespace Tests\Unit\Repositories;
 
+use Illuminate\Database\Eloquent\Collection;
+use IndieHD\Velkart\Contracts\ProductImageRepositoryContract;
 use IndieHD\Velkart\Contracts\ProductRepositoryContract;
 use IndieHD\Velkart\Tests\Unit\Repositories\RepositoryTestCase;
 
 class ProductTest extends RepositoryTestCase
 {
+    protected $productImage;
+
     public function setUp(): void
     {
         parent::setUp();
 
         $this->repo = resolve(ProductRepositoryContract::class);
+
+        $this->productImage = resolve(ProductImageRepositoryContract::class);
     }
 
     /** @test */
@@ -32,7 +38,16 @@ class ProductTest extends RepositoryTestCase
     {
         $product = $this->create();
 
-        $this->assertNotNull($product->images);
+        $images = factory($this->productImage->modelClass(), 2)->make();
+
+        $product->images()->saveMany($images);
+
+        $this->assertInstanceOf(Collection::class, $product->images);
+
+        $this->assertInstanceOf(
+            $this->productImage->modelClass(),
+            $product->images->first()
+        );
     }
 
     /** @test */
