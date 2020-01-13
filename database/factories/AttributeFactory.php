3cc8be40f8d36d2db70fb25a1161b9fb61561f7f
Poppabear <servnx@gmail.com>
2020-01-13 12:36:32 -0500

    fix(Factory): Name should be unique

--- database/factories/AttributeFactory.php
+++ database/factories/AttributeFactory.php
@@ -5,7 +5,7 @@ use IndieHD\Velkart\Models\Eloquent\Attribute;
 $factory->define(Attribute::class, function (Faker\Generator $faker) {
 
     return [
-        'name' => $faker->word
+        'name' => $faker->unique()->word
     ];
 
 });
