5143e83c0c60c0d4526ced391facee6f8073b8c9
Marcel Brautzsch <mb@35mm.me>
2020-01-14 00:06:09 +0100

    refact: make changes to database and model

--- src/Models/Eloquent/Category.php
+++ src/Models/Eloquent/Category.php
@@ -2,11 +2,23 @@
 
 namespace IndieHD\Velkart\Models\Eloquent;
 
+use Baum\NestedSet\Node;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
+    use Node;
+
     public $timestamps = false;
 
-    protected $guarded = ['id'];
+    protected $guarded = ['id', 'parent_id', 'left', 'right', 'depth'];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
