6a6f9af996b827b692fdabbe7c81fe35ef5a10b5
Poppabear <servnx@gmail.com>
2020-01-13 10:50:05 -0500

    tests(Attribute): Add additional tests w/ implementations

--- src/Models/Eloquent/Attribute.php
+++ src/Models/Eloquent/Attribute.php
@@ -3,10 +3,16 @@
 namespace IndieHD\Velkart\Models\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Attribute extends Model
 {
     public $timestamps = false;
 
     protected $guarded = ['id'];
+
+    public function values(): HasMany
+    {
+        return $this->hasMany(AttributeValue::class);
+    }
 }
